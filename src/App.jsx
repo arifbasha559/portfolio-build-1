454920b22566329b6df1e7ecb27773e0ffb2bafc
arifbasha559 <arifbasha559@gmail.com>
2024-01-23 19:15:29 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+
+const App = () => {
+  return (
+    <div className="scroll bg-[]">
+      <div className="h-screen heros">
+        
+      </div>
+      
+    </div>
+  );
+};
+
+export default App;
