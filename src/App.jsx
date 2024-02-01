256f9684cc50e450413acf36349a90fe925d3889
arifbasha559 <arifbasha559@gmail.com>
2024-02-01 21:20:01 +0530

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,22 @@ import Skill from "./Components/Skill";
 import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
 import { useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const App = () => {
-  let [inp,setInp]= useState("-translate-y-[200%]") ;
-  let [scr,setScr]= useState("scroll") ;
+  let [inp, setInp] = useState("-translate-y-[200%]");
+  let [scr, setScr] = useState("scroll");
   const inputa = () => {
-  if(inp !=="-translate-y-[200%]"){
-    setInp('-translate-y-[200%]')
-    setScr('asda')
-  }
-  else{
-    setInp('translat')
-    setScr('overflow-y-hidden h-screen')
-  }}
-  const con =inputa;
+    if (inp !== "-translate-y-[200%]") {
+      setInp("-translate-y-[200%]");
+      setScr("asda");
+    } else {
+      setInp("translat");
+      setScr("overflow-y-hidden h-screen");
+    }
+  };
+  const con = inputa;
   const [project] = useState([
     {
       pName: "Mak Todos",
@@ -31,7 +33,7 @@ const App = () => {
         "Mak Todos is a minimalist and user-friendly todo application designed to help you organize your tasks efficiently.",
       btn: ["Tailwind", "React Js"],
       pLink: "https://github.com/arifbasha559/mak-todos",
-      image: "./images/makTodos.png"
+      image: "./images/makTodos.png",
     },
     {
       pName: "Mak Todos",
@@ -39,39 +41,50 @@ const App = () => {
         "Mak Todos is a minimalist and user-friendly todo application designed to help you organize your tasks efficiently.",
       btn: ["Tailwind", "React Js"],
       pLink: "https://github.com/arifbasha559/mak-todos",
-      image: "./images/makTodos.png"
+      image: "./images/makTodos.png",
     },
   ]);
   useEffect(() => {
-    document.addEventListener('keydown', evt => {
-      if (evt.key === 'Escape' ) {
-          // alert('Escape pressed');
-          if(scr != 'scroll'){
-           con()
-          }
-          else{
-            return;
-          }
+    document.addEventListener("keydown", (evt) => {
+      if (evt.key === "Escape") {
+        // alert('Escape pressed');
+        if (scr != "scroll") {
+          con();
+        } else {
+          return;
+        }
       }
-  });
-    
-  }, [scr,con])
-  
+    });
+  }, [scr, con]);
+  // Animation
+  // useGSAP(() => {
+  //   const tl = gsap.timeline();
+  //   tl.from(".me", {
+  //     top: 20,
+  //     // duration: 3,
+  //     // color: "#000",
+  //   }).from("#hero", {
+  //     right: 500,
+  //     opacity: 0,
+  //     duration: 2,
+  //     stagger:1
+  //   });
+  // });
+  // Animation
   return (
     <div className={`scroll bg-[#041F31] ${scr} dh`}>
       {/* navbar */}
       <div className="max-w-screen-xl relative mx-auto h-full heros w-4/5  ">
         <Navbar create={inputa} />
         {/* navbar */}
-      
-       
+
         {/* heros */}
         <Hero create={inputa} />
         <div id="about" className=""></div>
         {/* heros */}
         <div className="max-w-screen-xl mx-auto flex flex-col gap-32 mt-20">
-          <div  className="">
-            <h2  className="px-4 text-xl text-white mb-5 font-semibold">
+          <div className="">
+            <h2 className="px-4 me text-xl text-white mb-5 font-semibold">
               About Me
             </h2>
             <div className="md:w-1/2 w-full rounded-md  bg-gradient-to-r from-[rgb(0,245,160)] to-[rgb(0,217,245)] bg-clip-padding">
@@ -79,13 +92,17 @@ const App = () => {
                 {" "}
                 a Front End Developer with a passion for software development.
                 My journey has been marked by a dedication to Proficient in
-                C,C++,Python,Javascript,Full Stack development. With a background in
-                BCA, I bring a unique perspective to Software Industry.
-          <span id="projects"></span>
+                C,C++,Python,Javascript,Full Stack development. With a
+                background in BCA, I bring a unique perspective to Software
+                Industry.
+                <span id="projects"></span>
               </p>
             </div>
           </div>
-          <h2 className="text-xl text-white font-semibold px-4 -mt-16" > Projects</h2>
+          <h2 className="text-xl text-white font-semibold px-4 -mt-16">
+            {" "}
+            Projects
+          </h2>
         </div>
         <div className="flex w-full justify-center flex-wrap gap-4 mt-4 mx-auto">
           {project.map((data, index) => {
@@ -112,11 +129,9 @@ const App = () => {
         {/* skill */}
         <h2 className="text-xl text-white font-semibold px-4 mb-5 ">Skills</h2>
         <div className="flex mx-auto w-11/12 text-sm md:text-lg md:w-4/5 flex-wrap gap-20">
-
           <Skill />
-
         </div>
-         <div className="w-full flex justify-center mb-10 mt-5">
+        <div className="w-full flex justify-center mb-10 mt-5">
           <a
             href="https://github.com/arifbasha559/"
             className=" self-center my-4 text-[#041F31] font-medium py-2 px-6 text-center rounded-md hover:opacity-80 bg-gradient-to-r from-[rgb(0,245,160)] to-[rgb(0,217,245)] "
@@ -124,13 +139,12 @@ const App = () => {
             Know more
           </a>
         </div>
-        <div className="footer "  >
+        <div className="footer ">
           <Footer create={inputa} />
         </div>
-        
       </div>
       <div className="w-screen bg-black">
-        <Contact  toggle={inp} create={inputa} />
+        <Contact toggle={inp} create={inputa} />
       </div>
     </div>
   );
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,15 +6,15 @@ const Hero = (props) => {
   return (
     <div className="py-10">
 
-          <section className="bg-inherit  flex justify-center items-center" >
+          <section className="bg-inherit  flex justify-center items-center relative"  >
             <div className=" relative flex flex-col-reverse gap-10 md:grid max-w-screen-xl py-8 mx-auto  lg:gap-8 xl:gap-0 lg:py-16  lg:grid-cols-12">
-              <div className="mr-auto place-self-center lg:col-span-7">
-                <p className="max-w-2xl mb-4 font-normal  md:text-md lg:text-lg text-gray-400">Hello! I&rsquo;m </p>
-                <h1 className="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl bg-gradient-to-r from-[rgb(0,245,160)] to-[rgb(0,217,245)] bg-clip-text text-transparent ">ARIF BASHA .H</h1>
-                <p className="max-w-xl tracking-wider mb-6 font-light  lg:mb-8 md:text-md lg:text-md text-gray-300"> a Web Developer with a passion for software development. My journey has been marked by a dedication to Proficient in C,C++,Python,Javascript,Full Stack Development. With a background in BCA, I bring a unique perspective to Software Industry.</p>
-                <button
+              <div className="mr-auto place-self-center relative lg:col-span-7">
+                <p className="max-w-2xl relative mb-4 font-normal  md:text-md lg:text-lg text-gray-400" id='hero'>Hello! I&rsquo;m </p>
+                <h1 className="relative max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl bg-gradient-to-r from-[rgb(0,245,160)] to-[rgb(0,217,245)] bg-clip-text text-transparent " id='hero' >ARIF BASHA .H</h1>
+                <p className="relative max-w-xl tracking-wider mb-6 font-light  lg:mb-8 md:text-md lg:text-md text-gray-300 me" id='hero'> a Web Developer with a passion for software development. My journey has been marked by a dedication to Proficient in C,C++,Python,Javascript,Full Stack Development. With a background in BCA, I bring a unique perspective to Software Industry.</p>
+                <button id='hero'
                   onClick={()=>{props.create()}}
-                  className="text-[#041F31] font-medium py-3 px-6 rounded-md hover:opacity-80 bg-gradient-to-r from-[rgb(0,245,160)] to-[rgb(0,217,245)] "
+                  className="relative text-[#041F31] font-medium py-3 px-6 rounded-md hover:opacity-80 bg-gradient-to-r from-[rgb(0,245,160)] to-[rgb(0,217,245)] "
                 >
                   Contact
                 </button>
@@ -23,7 +23,7 @@ const Hero = (props) => {
                 <img className="" src="https://www.ahmedazmy.xyz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcoder1.3b970e5d.png&w=1920&q=75" alt="mockup" />
               </div>
             </div>
-            <a className="absolute p-4 bottom-5 hover:bg-[rgba(0,0,0,0.27)] rounded-full " href="#about" >
+            <a className="absolute p-4 lg:bottom-5 -bottom-5 hover:bg-[rgba(0,0,0,0.27)] rounded-full " href="#about" >
               <IconContext.Provider value={{ color: "white", className: "global-class-name" }}>
                 <FaArrowDown />
               </ IconContext.Provider>
